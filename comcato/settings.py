af883143751072a04a771569bec97a77b23a5f5d
tariq628 <tariqkaleri628@gmail.com>
2024-05-10 09:32:13 +0500

    Added django environ

--- comcato/settings.py
+++ comcato/settings.py
@@ -1,10 +1,18 @@
 from pathlib import Path
 import os
+from os.path import dirname, join
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+local_env = join(dirname(__file__), ".env.development")
+if os.path.exists(local_env):
+    environ.Env.read_env(env_file=local_env)
 
+# Loading all environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -133,6 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
-DEFAULT_FROM_EMAIL = ""
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
