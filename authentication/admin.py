de66b94fd2f88415cb22b7577e2872f980120f03
tariq628 <tariqkaleri628@gmail.com>
2024-05-10 10:06:15 +0500

    Site matching query doesn't exist error

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Language,Material,Userdetail,Payment , Interest , Favouritetopic
+from .models import Language, Material, Userdetail, Payment, Interest, Favouritetopic
+
 
 class MaterialForm(admin.ModelAdmin):
-    list_display = ('your_language','foreign_language', 'file', 'name','type')
+    list_display = ('your_language', 'foreign_language', 'file', 'name', 'type')
     search_fields = ('language__name', 'name')
 
-admin.site.register(Material,MaterialForm)
+
+admin.site.register(Material, MaterialForm)
 admin.site.register(Language)
 admin.site.register(Userdetail)
 admin.site.register(Payment)
--- authentication/views.py
+++ authentication/views.py
@@ -1,25 +1,21 @@
 from django.shortcuts import *
-from .form import LoginForm,RegistrationForm
+from .form import LoginForm, RegistrationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login as django_login , logout as django_logout
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from .models import Userdetail
 import uuid
 from django.contrib.auth.models import User
 from .utils import *
-from django.contrib.sites.models import Site
 
 # Create your views here.
 
 
-
 def login(request):
-
-  
 
     Login_form = LoginForm()
     Register_form = RegistrationForm()
 
-    return render(request,'login.html',{'title':'Login','Login_form':Login_form,'Register_form':Register_form,})
+    return render(request, 'login.html', {'title': 'Login', 'Login_form': Login_form, 'Register_form': Register_form, })
 
 
 def login_validation(request):
@@ -31,68 +27,67 @@ def login_validation(request):
 
             # Authenticate user
             user = authenticate(request, username=username, password=password)
-           
+
             if user is not None:
-                obj = Userdetail.objects.get(user = user.id)
-                if(obj.is_verify):
+                obj = Userdetail.objects.get(user=user.id)
+                if (obj.is_verify):
 
                     django_login(request, user)
-                    return redirect('home')  
+                    return redirect('home')
                 else:
                     messages.error(request, 'Account is not verify')
             else:
                 messages.error(request, 'Invalid username or password.')
     Login_form = LoginForm()
     Register_form = RegistrationForm()
-    return render(request,'login.html',{'title':'Login','Login_form':Login_form,'Register_form':Register_form,})
+    return render(request, 'login.html', {'title': 'Login', 'Login_form': Login_form, 'Register_form': Register_form, })
+
 
 def add_user(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-           
+
             username = form.cleaned_data['username']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-          
-
 
             if User.objects.filter(email=email).exists():
                 messages.error(request, 'Email address is already in use.')
                 return redirect('login')
-            
+
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'UserName address is already in use.')
                 return redirect('login')
-            
-           
-            user = User.objects.create_user(username, email,password)
+
+            user = User.objects.create_user(username, email, password)
             # # Save the user to the database with the hashed password
             # # Create a RegistrationForm RegistrationForm
             userdetail_form_instance = Userdetail(
-                user = user,
-                email_token =str(uuid.uuid4())
+                user=user,
+                email_token=str(uuid.uuid4())
             )
             userdetail_form_instance.save()
 
-            a = send_email_token(email , userdetail_form_instance.email_token)
+            a = send_email_token(email, userdetail_form_instance.email_token)
             messages.success(request, 'Registration successful. Please login.')
             # Perform necessary actions with the data
     Login_form = LoginForm()
     Register_form = RegistrationForm()
-    return render(request,'login.html',{'title':'Login','Login_form':Login_form,'Register_form':Register_form,})
+    return render(request, 'login.html', {'title': 'Login', 'Login_form': Login_form, 'Register_form': Register_form, })
+
 
 def logout(request):
     django_logout(request)
     return redirect('home')
 
-def verify(request,token):
-    try:
-        
 
-        obj = Userdetail.objects.get(email_token = token)
-        obj.is_verify=True
+def verify(request, token):
+    try:
+
+        obj = Userdetail.objects.get(email_token=token)
+        obj.is_verify = True
         obj.save()
-        return render(request,'verify.html',{'messages':'Your account is verify'})
+        return render(request, 'verify.html', {'messages': 'Your account is verify'})
     except Exception as e:
-        return render(request,'verify.html',{'messages':'Your account is not verify'})
+        return render(request, 'verify.html', {'messages': 'Your account is not verify'})
